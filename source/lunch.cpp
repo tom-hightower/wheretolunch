3a0753795aa773ada1522db2618462620203de7b
Tom Hightower <tom.hightower356@gmail.com>
2019-04-01 21:49:42 -0400

    http curl requests work now

--- source/lunch.cpp
+++ source/lunch.cpp
@@ -1,9 +1,88 @@
 #include <iostream>
+#include <cstdint>
+#include <memory>
 #include <string>
 #include <vector>
 #include "curl/curl.h"
+#include "json/json.h"
+#include "secret_config.h"
 
 using namespace std;
+namespace
+{
+    size_t callback(
+        const char* in,
+        size_t size,
+        size_t num,
+        string* out)
+    {
+        const size_t totalBytes(size * num);
+        out->append(in, totalBytes);
+        return totalBytes;
+    }
+}
+
+static int get_curl_json(string url) {
+    CURL* curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER , 1);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST , 1);
+
+
+    // Set remote URL.
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+    // Response information.
+    int httpCode(0);
+    unique_ptr<string> httpData(new string());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+
+    // Run our HTTP GET command, capture the HTTP response code, and clean up.
+    curl_easy_perform(curl);
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+    curl_easy_cleanup(curl);
+
+    if (httpCode == 200)
+    {
+        cout << "\nGot successful response from " << url << endl;
+
+        Json::Value jsonData;
+        Json::Reader jsonReader;
+
+        if (jsonReader.parse(*httpData.get(), jsonData))
+        {
+            cout << "Successfully parsed JSON data" << endl;
+            cout << "\nJSON data received:" << endl;
+            cout << jsonData.toStyledString() << endl;
+
+            const string dateString(jsonData["date"].asString());
+            const size_t unixTimeMs(
+                jsonData["milliseconds_since_epoch"].asUInt64());
+            const string timeString(jsonData["time"].asString());
+
+            cout << "Natively parsed:" << endl;
+            cout << "\tDate string: " << dateString << endl;
+            cout << "\tUnix timeMs: " << unixTimeMs << endl;
+            cout << "\tTime string: " << timeString << endl;
+            cout << endl;
+        }
+        else
+        {
+            cout << "Could not parse HTTP data as JSON" << endl;
+            cout << "HTTP data was:\n" << *httpData.get() << endl;
+            return 1;
+        }
+    }
+    else
+    {
+        cout << "Couldn't GET from " << url << " - exiting" << endl;
+        return 1;
+    }
+}
 
 static void show_usage(string name) {
     cerr << "Usage: " << name << " <option(s)>"
@@ -17,8 +96,10 @@ int main(int argc, char* argv[]) {
         show_usage(argv[0]);
         return 1;
     }
-    cout << "Hello World!\n";
-
+    const string ipUrl = (string("http://ipinfo.io?token=") + IPINFO_API_KEY);
+    const string placeUrl = (string("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Gatech&inputtype=textquery&fields=formatted_address,place_id,name,permanently_closed,types&key=") + GOOGLE_API_KEY);
+    auto thing = get_curl_json(ipUrl);
+    auto thing2 = get_curl_json(placeUrl);
 
     return 0;
 }
